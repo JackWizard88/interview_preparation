efe2ee15a12349e14f330b095bf85c4cdafce6bb
Andrei Krylov <krilovavit@mail.ru>
2021-02-05 16:02:14 +0300

    hw2 честно скажу, подглядывал, и долго мучался с ArrayStoreException потому что почему то конструктор импортировал Object из какого то пакета Cobra..

---
+++ src/main/java/ru/geekbrains/interveiw_preparation/hw2/linkedList/LinkedList.java
@@ -0,0 +1,36 @@
+package ru.geekbrains.interveiw_preparation.hw2.linkedList;
+
+public interface LinkedList<E> {
+
+    void insertFirst(E value);
+
+    E removeFirst();
+
+    boolean remove(E value);
+
+    boolean contains(E value);
+
+    int size();
+
+    boolean isEmpty();
+
+    void display();
+
+    E getFirst();
+
+    class Node<E> {
+
+        E item;
+        Node<E> next;
+
+        public Node(E item, Node<E> next) {
+            this.item = item;
+            this.next = next;
+        }
+
+        public E getItem() {
+            return item;
+        }
+    }
+
+}
